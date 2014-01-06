38c14cd2782d2431586f7a14ce199777fadf12de
Christopher Adams <chris@raysend.com>
2014-01-07 02:51:22 +0800

    WIP: Start working on reforms component

--- jsx/reforms.jsx
+++ jsx/reforms.jsx
@@ -3,13 +3,61 @@
  */
 
 var Reforms = React.createClass({
+  getInitialState: function() {
+    var apiVersion = 'b1';
+    var reformsURL = '/us/' + apiVersion;
+
+    $.ajax({
+      url: reformsURL,
+      success: function(data) {
+        console.log(data);
+      }.bind(this)
+    });
+
+    return { reforms: [] };
+  },
   render: function() {
     return (
       <div className="ac-reforms">
         <div className="row">
           <div className="medium-6 columns">
+            <ReformsList reforms={this.state.reforms} />
           </div>
         </div>
+      </div>
+    );
+  }
+});
+
+var ReformsList = React.createClass({
+  getInitialState: function() {
+    return { reforms: [] }
+  },
+  componentWillReceiveProps: function(props) {
+  },
+  render: function() {
+    var reformNodes = [];
+    /*
+    var reformNodes = this.props.reforms.map(function (reform) {
+      return <Reform
+        key={reform.id}
+        title={reform.title}
+        />
+    });
+    */
+    return (
+      <div className="ac-reform-list">
+        {reformNodes}
+      </div>
+    );
+  }
+});
+
+var Reform = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Reform</p>
       </div>
     );
   }
