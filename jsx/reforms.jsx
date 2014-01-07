4198405fed141fcda180d71203d2a4ccb016eee4
Christopher Adams <chris@raysend.com>
2014-01-07 16:15:20 +0800

    Display reforms on home page

--- jsx/reforms.jsx
+++ jsx/reforms.jsx
@@ -4,13 +4,13 @@
 
 var Reforms = React.createClass({
   getInitialState: function() {
-    var apiVersion = 'b1';
+    var apiVersion = 'b2';
     var reformsURL = '/us/' + apiVersion;
 
     $.ajax({
       url: reformsURL,
       success: function(data) {
-        console.log(data);
+        this.setState({ reforms: data.reforms });
       }.bind(this)
     });
 
@@ -20,7 +20,7 @@ var Reforms = React.createClass({
     return (
       <div className="ac-reforms">
         <div className="row">
-          <div className="medium-6 columns">
+          <div className="large-6 large-offset-3 medium-10 medium-offset-1 columns">
             <ReformsList reforms={this.state.reforms} />
           </div>
         </div>
@@ -36,17 +36,20 @@ var ReformsList = React.createClass({
   componentWillReceiveProps: function(props) {
   },
   render: function() {
-    var reformNodes = [];
-    /*
     var reformNodes = this.props.reforms.map(function (reform) {
+      var version = reform['1.0'];
       return <Reform
         key={reform.id}
-        title={reform.title}
+        title={version.title}
+        description={version.description}
+        sponsor={version.sponsor}
+        billId={version.bill_id}
+        url={version.url}
         />
     });
-    */
     return (
       <div className="ac-reform-list">
+        <h4 className="subheader text-center">Reforms</h4>
         {reformNodes}
       </div>
     );
@@ -57,7 +60,18 @@ var Reform = React.createClass({
   render: function() {
     return (
       <div>
-        <p>Reform</p>
+        <h3>{this.props.title}</h3>
+        <p>
+          <strong>
+            {this.props.description}.{' '}
+          </strong>
+          {this.props.sponsor.name ? "Sponsored by " + this.props.sponsor.name + '.'  : ''}{' '}
+          <a href={this.props.url}>
+            {this.props.url ? "Learn More" : ''}
+          </a>
+            {this.props.url ? "." : ''}
+        </p>
+        <hr/>
       </div>
     );
   }
