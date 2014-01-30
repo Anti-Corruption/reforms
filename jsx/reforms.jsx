9accdefd5d86063c378e07453486ca17d2e5fcb4
Christopher Adams <chris@raysend.com>
2014-01-30 17:19:05 -0500

    Display reforms organized by type

--- jsx/reforms.jsx
+++ jsx/reforms.jsx
@@ -10,18 +10,44 @@ var Reforms = React.createClass({
     $.ajax({
       url: reformsURL,
       success: function(data) {
-        this.setState({ reforms: data.reforms });
+
+        // Organize the reforms by type
+        var reforms_by_type = {};
+
+        data.reforms.forEach(function(element, index, array) {
+          var version = element['1.0'];
+          var type = version.reform_type;
+
+          // Create an object to hold reforms for this type
+          if (!reforms_by_type.hasOwnProperty(type)) {
+            reforms_by_type[type] = { type: type, reforms: [] }
+          }
+
+          reforms_by_type[type].reforms.push(element);
+
+        });
+
+        // Turn the Object back into a plain array
+        var reforms = [];
+        for (key in reforms_by_type) {
+          reforms.push(reforms_by_type[key]);
+        }
+
+        this.setState({ reforms: reforms });
       }.bind(this)
     });
 
     return { reforms: [] };
   },
   render: function() {
+    var reformsListNodes = this.state.reforms.map(function (reformList) {
+      return <ReformsList key={reformList.type} reforms={reformList.reforms} />
+    });
     return (
       <div className="ac-reforms">
         <div className="row">
           <div className="large-6 large-offset-3 medium-10 medium-offset-1 columns">
-            <ReformsList reforms={this.state.reforms} />
+            {reformsListNodes}
           </div>
         </div>
       </div>
@@ -50,7 +76,7 @@ var ReformsList = React.createClass({
     });
     return (
       <div className="ac-reform-list">
-        <h4 className="subheader text-center">Reforms</h4>
+        <h4 className="subheader text-center">{this.props.key}</h4>
         {reformNodes}
       </div>
     );
@@ -59,18 +85,9 @@ var ReformsList = React.createClass({
 
 var Reform = React.createClass({
   render: function() {
-	headerStyle = {
-	lineHeight: 1
-	};
-	typeStyle = {
-	textTransform: 'uppercase'
-	};
     return (
       <div>
-        <h3 style={headerStyle}>
-			{this.props.title} {' '}
-			<small style={typeStyle}>{this.props.type} { ' ' } Reform</small>
-		</h3>
+        <h3>{this.props.title}</h3>
         <p>
           <strong>
             {this.props.description}.{' '}
